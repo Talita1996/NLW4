2b2ee34db31e7134a43471bcd6937fb4c0413d62
Talita Mota <talita.santos@otimizeit.com.br>
2021-03-08 11:47:53 -0300

    Errors fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "73ba9df5316949131833";
+/******/ 	var hotCurrentHash = "9ac77bc6bc75e19889eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+/*
 export default class MyDocument {
+
     render() {
         return (
             <Html>
@@ -15,4 +16,27 @@ export default class MyDocument {
             </ Html>
         );
     }
+} */
+
+function MyDocument() {
+
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" /> 
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </ Html>
+    );
+    
 }
+
+MyDocument.getInitialProps = Document.getInitialProps;
+
+MyDocument.renderDocument = Document.renderDocument;
+
+export default MyDocument;
